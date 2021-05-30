64b8ea4fe7cbbf036927cdde46ae43f721ccac5c
Revision <tkn963@gmail.com>
2021-05-30 16:14:42 +0200

    Interfaced
    Added events to player frame to handle vehicles. It performs a wait before updating position. The current wait time is half a second. Leaving the slash command for occasions where it's needed.

--- Interfaced/Interfaced.lua
+++ Interfaced/Interfaced.lua
@@ -1,3 +1,36 @@
+local waitTable = {};
+local waitFrame = nil;
+
+function Interfaced_Wait(delay, func, ...)
+    if (type(delay)~="number" or type(func)~="function") then
+        return false;
+    end
+
+    if (waitFrame == nil) then
+        waitFrame = CreateFrame("Frame","WaitFrame", UIParent);
+        waitFrame:SetScript("onUpdate",function (self,elapse)
+            local count = #waitTable;
+            local i = 1;
+            while (i<=count) do
+                local waitRecord = tremove(waitTable,i);
+                local d = tremove(waitRecord,1);
+                local f = tremove(waitRecord,1);
+                local p = tremove(waitRecord,1);
+                if (d>elapse) then
+                    tinsert(waitTable,i,{d-elapse,f,p});
+                    i = i + 1;
+                else
+                    count = count - 1;
+                    f(unpack(p));
+                end
+            end
+        end);
+    end
+
+    tinsert(waitTable,{delay,func,{...}});
+    return true;
+end
+
 local function main()
     PlayerFrame:ClearAllPoints()
     PlayerFrame:SetPoint("CENTER", -350, 100)
@@ -36,9 +69,15 @@ local ef = CreateFrame("FRAME")
     ef:RegisterEvent("PLAYER_ENTERING_WORLD")
     ef:SetScript("OnEvent", function(self)
     main()
-    main = nil
     self:UnregisterEvent("PLAYER_ENTERING_WORLD")
     self:SetScript("OnEvent", nil)
+end)
+
+local uf = CreateFrame("FRAME")
+    uf:RegisterEvent("UNIT_ENTERED_VEHICLE")
+    uf:RegisterEvent("UNIT_EXITED_VEHICLE")
+    uf:SetScript("OnEvent", function(self)
+    Interfaced_Wait(0.5, main);
 end)
 
 SLASH_UPDATEUI1 = "/updateui"
